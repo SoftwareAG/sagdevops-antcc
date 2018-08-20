7ff3b4a50fdde362dbcda4347bb117dd6b45d7bb
BPAS <BPAS@MCBPAS01.eur.ad.sag>
2018-08-20 16:09:55 +0300

    try to upload release file

---
+++ deploy/pack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+# Execute only on linux and skip osx
+if [ `uname` = "Linux" ]
+then
+	cd $HOME
+	#setting default values
+	CC_VERSION=${CC_VERSION:-10.3-stable}
+	CC_DISTRO=${CC_DISTRO:-antcc-nojava}
+	# set distribution filename
+	CC_DISTRO_FILENAME="$CC_DISTRO-$CC_VERSION.zip"
+	echo "Currently in folder `pwd`"
+	echo "Environment variables values:"
+	env
+	echo
+	echo "zipping built project to  $CC_DISTRO_FILENAME"
+	zip -r $CC_DISTRO_FILENAME ./.sag/tools/CommandCentral ./.sag/tools/common ./.sag/tools/sagdevops-antcc
+fi
