76c004f7e8058397552f6c582d17c87acd3e9332
BPAS <BPAS@MCBPAS01.eur.ad.sag>
2018-08-20 14:29:08 +0300

    initial test for deployment.

---
+++ deploy/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Execute only on linux and skip osx
+if [ `uname` = "Linux" ]
+then
+	cd $HOME
+	#setting default values
+	CC_VERSION=${CC_VERSION:-10.3-stable}
+	CC_DISTRO=${CC_DISTRO:-antcc-nojava}
+	# set distribution filename
+	CC_DISTRO_FILENAME="$CC_DISTRO-$CC_VERSION"
+	echo "Currently in folder `pwd`"
+	echo "Environment variables values:"
+	env
+	echo
+	echo "zipping built project to  "
+	zip -r $CC_DISTRO_FILENAME ./sag/tools/CommandCentral ./sag/tools/common ./sag/tools/sagdevops-antcc
+fi
