aa89bbef563bda3c62809a96d58f738e64efb8e9
BPAS <BPAS@MCBPAS01.eur.ad.sag>
2018-10-10 17:51:15 +0300

    set version and distroname vie env vars

--- deploy/pack.sh
+++ deploy/pack.sh
@@ -23,15 +23,12 @@ then
         pwd
 	cd $HOME
 	#setting default values
-#	CC_VERSION=${CC_VERSION:-10.3-stable}
-#	CC_DISTRO=${CC_DISTRO:-antcc-nojava}
+	CC_VERSION=${CC_VERSION:-10.3-stable}
+	CC_DISTRO=${CC_DISTRO:-antcc-nojava}
 	# set distribution filename
-#	CC_DISTRO_FILENAME="$CC_DISTRO-$CC_VERSION-any.zip"
-	CC_DISTRO_FILENAME="antcc-nojava-10.3-stable-any.zip"
-#	echo "Currently in folder `pwd`"
-#	echo "Environment variables values:"
-#	env
-#	echo
+	CC_DISTRO_FILENAME="$CC_DISTRO-$CC_VERSION-any.zip"
+#	CC_DISTRO_FILENAME="antcc-nojava-10.3-stable-any.zip"
+
 	echo "zipping built project to  $HOME/$CC_DISTRO_FILENAME"
 	zip -r $HOME/$CC_DISTRO_FILENAME ./.sag/tools/CommandCentral ./.sag/tools/common ./.sag/tools/sagdevops-antcc
 fi
