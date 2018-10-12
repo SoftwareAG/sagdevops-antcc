3a96dd095391f3e9a96c6020433af5af1e62b5f3
bpaskalev <34301461+bpaskalev@users.noreply.github.com>
2018-10-12 11:24:29 +0300

    Update pack.sh

--- deploy/pack.sh
+++ deploy/pack.sh
@@ -23,12 +23,10 @@ then
         pwd
 	cd $HOME
 	#setting default values
-	CC_VERSION=${CC_VERSION:-10.3-stable}
-	CC_DISTRO=${CC_DISTRO:-antcc-nojava}
+	ANTCC_VERSION=${ANTCC_VERSION:-10.3-stable}
+	ANTCC_DISTRO=${ANTCC_DISTRO:-antcc-nojava}
 	# set distribution filename
-	CC_DISTRO_FILENAME="$CC_DISTRO-$CC_VERSION-any.zip"
-#	CC_DISTRO_FILENAME="antcc-nojava-10.3-stable-any.zip"
-
-	echo "zipping built project to  $HOME/$CC_DISTRO_FILENAME"
-	zip -r $HOME/$CC_DISTRO_FILENAME ./.sag/tools/CommandCentral ./.sag/tools/common ./.sag/tools/sagdevops-antcc
+	ANTCC_DISTRO_FILENAME="$ANTCC_DISTRO-$ANTCC_VERSION-any.zip"
+	echo "zipping built project to  $HOME/$ANTCC_DISTRO_FILENAME"
+	zip -r $HOME/$ANTCC_DISTRO_FILENAME ./.sag/tools/CommandCentral ./.sag/tools/common ./.sag/tools/sagdevops-antcc
 fi
